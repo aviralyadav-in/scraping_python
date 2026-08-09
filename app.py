746b56173860363e804e4e7adb043efc4a522fc0
Arpita <arpitapandey0012@gmail.com>
2026-08-09 19:41:04 +0530

    Add Flask application entry point

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from api import register_routes
+
+app = Flask(__name__)
+
+register_routes(app)
+
+if __name__ == "__main__":
+    app.run(
+        host="127.0.0.1",
+        port=5000,
+        debug=True
+    )
